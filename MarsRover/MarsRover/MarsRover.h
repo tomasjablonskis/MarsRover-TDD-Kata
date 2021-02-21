d6904ba959c9654c8b6a78d0b8350cc28fad279f
Tomas Jablonskis <tomas.jablonskis6@gmail.com>
2021-02-21 21:35:20 +0200

    Mars Rover TDD Kata init

---
+++ MarsRover/MarsRover/MarsRover.h
@@ -0,0 +1,18 @@
+//
+//  MarsRover.h
+//  MarsRover
+//
+//  Created by Tomas Jablonskis on 2021-02-21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarsRover.
+FOUNDATION_EXPORT double MarsRoverVersionNumber;
+
+//! Project version string for MarsRover.
+FOUNDATION_EXPORT const unsigned char MarsRoverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarsRover/PublicHeader.h>
+
+
---
+++ MarsRover/MarsRoverTests/MarsRoverTests.swift
@@ -0,0 +1,14 @@
+//
+//  MarsRoverTests.swift
+//  MarsRoverTests
+//
+//  Created by Tomas Jablonskis on 2021-02-21.
+//
+
+import XCTest
+import MarsRover
+
+class MarsRoverTests: XCTestCase {
+
+
+}
