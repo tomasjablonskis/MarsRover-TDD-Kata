99910780a1128511182c34999113053240be496c
Tomas Jablonskis <tomas.jablonskis6@gmail.com>
2021-02-22 20:46:06 +0200

    Rover init sets initial values

---
+++ MarsRover/MarsRover/Rover.swift
@@ -0,0 +1,29 @@
+//
+//  Rover.swift
+//  MarsRover
+//
+//  Created by Tomas Jablonskis on 2021-02-22.
+//
+
+import Foundation
+
+public struct Coordinate: Equatable {
+    public var x: Int
+    public var y: Int
+
+    public init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+public class Rover {
+
+    private(set) public var coordinate: Coordinate
+    private(set) public var direction: String
+
+    public init(coordinate: Coordinate, direction: String) {
+        self.coordinate = coordinate
+        self.direction = direction
+    }
+}
--- MarsRover/MarsRoverTests/MarsRoverTests.swift
+++ MarsRover/MarsRoverTests/MarsRoverTests.swift
@@ -5,10 +5,19 @@
 //  Created by Tomas Jablonskis on 2021-02-21.
 //
 
+import Foundation
 import XCTest
 import MarsRover
 
 class MarsRoverTests: XCTestCase {
 
+    func test_init_setsInitialValues() {
+        let initialCoordinate = Coordinate(x: 0, y: 0)
+        let initialDirection = "W"
+        let rover = Rover(coordinate: initialCoordinate, direction: initialDirection)
+
+        XCTAssertEqual(rover.coordinate, initialCoordinate)
+        XCTAssertEqual(rover.direction, initialDirection)
+    }
 
 }
